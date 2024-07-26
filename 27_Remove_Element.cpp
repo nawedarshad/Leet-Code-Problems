f035cbcf377b0048216b47a482d18f2fb1a120e0
Mohammad Nawed Arshad <116885779+nawedarshad@users.noreply.github.com>
2024-07-26 14:28:33 +0530

    Create 27_Remove_Element.cpp

---
+++ 27_Remove_Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int newSize=0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i]!=val){
+                nums[newSize++] = nums[i];
+            }
+        } 
+        return newSize;
+    }
+};
